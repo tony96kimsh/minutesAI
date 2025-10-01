b779ba7364800c0c726e5008f2af0f01a8056f86
Sung Hoon Kim <tony96kimsh@gmail.com>
2025-10-02 05:43:59 +0900

    fix: change wrong file name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # main.py
-import WhisperModel 
+import whisper_model
 import FileHelper
 
 # 오디오 탐색기 호출
@@ -7,9 +7,9 @@ path = FileHelper.select_audio()
 
 if path != None:
     # Whisper 모델 불러오기
-    model = WhisperModel.load_whisper_model("small")
+    model = whisper_model.load_whisper_model("small")
     # 음성 파일 변환
-    stt_text = WhisperModel.transcribe_audio(model, path)
+    stt_text = whisper_model.transcribe_audio(model, path)
 
     print("변환 텍스트: ", stt_text)
 else:
